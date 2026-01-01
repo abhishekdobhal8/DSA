3cc437ebb99f0f747b78d0be264215ec3b7c1a7f
AbhIshek Dobhal <124232126+abhishekdobhal8@users.noreply.github.com>
2026-01-01 17:28:28 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (50.17%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -2,19 +2,6 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
         int n = digits.size();
-        vector<int> res;
-        int i = 0;
-        for(i=0; i<n; i++){
-            if(digits[i] != 9) break;
-        }
-
-        if(i == n) {
-            res.push_back(1);
-            for(int j=0; j<n; j++){
-                res.push_back(0);
-            }
-            return res;
-        }
 
         for(int j=n-1; j>=0; j--){
             if(digits[j] == 9) digits[j] = 0;
@@ -23,6 +10,10 @@ public:
                 return digits;
             }
         }
+
+        digits.insert(digits.begin(), 1);
+        return digits;
+
         return {};
     }
 };
